4c2fb28bd518ae90ed6a89777ac513a59fb14580
bucko13 <bucko.perley@gmail.com>
2018-11-11 19:36:15 -0800

    add full functionality for faucet

--- server/endpoints.js
+++ server/endpoints.js
@@ -3,12 +3,22 @@ const handlers = require('./handlers');
 module.exports = [
   {
     method: 'USE',
-    path: '/faucet/drip',
+    path: '/:client/faucet/drip',
     handler: handlers.apiLimiter
   },
   {
+    method: 'USE',
+    path: '/clients/:id/wallet/wallet/',
+    handler: handlers.blacklistWallets
+  },
+  {
     method: 'POST',
-    path: '/faucet/drip',
+    path: '/:client/faucet/drip',
     handler: handlers.requestFunds
+  },
+  {
+    method: 'GET',
+    path: '/:client/faucet/info',
+    handler: handlers.getFaucetInfo
   }
 ];
--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
+function getFaucetPath(clientId, walletId) {
+  return `/clients/${clientId}/wallet/wallet/${walletId}/`;
+}
+
 const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 15 minutes
   max: 1,
@@ -13,11 +17,62 @@ const apiLimiter = rateLimit({
   }
 });
 
-function requestFunds(req, res) {
-  return res.status(200).json(req.body);
+// precaution to ensure that only requests to a wallet
+// with the id "faucet" can be queried if not being run as faucet admin
+// (currently admin is just a bool set via configs);
+function blacklistWallets(req, res, next) {
+  // allow custom faucet id to be set at runtime via app config
+  // defaults to 'faucet'
+  const config = req.config;
+  const faucetWallet = config.str('faucet-id', 'faucet');
+
+  // note that if this is true, then all wallet endpoints are whitelisted
+  // use only for development purposes
+  const faucetAdmin = config.bool('faucet-admin', false);
+  if (!req.path.match(faucetWallet) && !faucetAdmin)
+    return res.status(403).json({ error: { message: 'Forbidden', code: 403 } });
+  next();
+}
+
+function requestFunds(req, res, next) {
+  const {
+    config,
+    logger,
+    params: { client }
+  } = req;
+  const faucetWallet = config.str('faucet-id', 'faucet');
+  logger.info(
+    'Sending faucet request to client "%s" and wallet "%s"',
+    client,
+    faucetWallet
+  );
+  const options = {
+    outputs: [
+      {
+        address: req.body.address,
+        value: 1000
+      }
+    ],
+    rate: req.body.rate
+  };
+  req.body = options;
+  req.url = `${getFaucetPath(client, faucetWallet)}/send`;
+  next();
+}
+
+function getFaucetInfo(req, res, next) {
+  const {
+    config,
+    params: { client }
+  } = req;
+  const faucetWallet = config.str('faucet-id', 'faucet');
+  req.url = `${getFaucetPath(client, faucetWallet)}/account/default`;
+  next();
 }
 
 module.exports = {
   requestFunds,
-  apiLimiter
+  apiLimiter,
+  blacklistWallets,
+  getFaucetInfo
 };
